b2a73b33df24cf7869eaab3181f9c446864e68bb
Cory Kane <ckane30691@gmail.com>
2017-08-03 22:00:26 -0700

    Add map and queue class

--- W1D5/adts.rb
+++ W1D5/adts.rb
@@ -15,4 +15,59 @@ class Stack
 	def show
 		@my_stack
 	end
+
+end
+
+class Queue
+
+	def initialize
+		@my_queue = []
+	end
+
+	def enqueue(val)
+		@my_queue.unshift(val)
+	end
+
+	def dequeue
+		@my_queue.pop
+	end
+
+	def show
+		@my_queue
+	end
+
+end
+
+class Map
+
+	def initialize
+		@my_map = []
+	end
+
+	def assign(k, v)
+		if @my_map.empty?
+			@my_map << [k, v]
+		else
+		  @my_map.each do |sub_arr|
+				if sub_arr[0] == k
+					sub_arr[1] = v
+					return self
+				end
+			end
+			@my_map << [k, v]
+		end
+	end
+
+	def lookup(k)
+		@my_map.each { |sub_arr| return sub_arr[1] if sub_arr[0] == k }
+	end
+
+	def remove(k)
+		@my_map.delete_if { |sub_arr| sub_arr[0] == k }
+	end
+
+	def show
+		@my_map
+	end
+
 end
